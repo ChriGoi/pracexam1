b8feca4c3e8069ae4c7a9253059811ee0f06f9e3
Christian <christian.goiak@outlook.com>
2023-03-16 11:28:16 +1030

    jhf

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -2,11 +2,11 @@
 int integer = 0;
 int bin_to_int(int binary_digits[5], int number_of_digits)
 {
-    int runningpower = number_of_digits;
+    int runningpower = number_of_digits - 1;
     for (int i = 0; i < number_of_digits; i++)
     {
         integer = integer + (binary_digits[i] * (2 ^ runningpower));
         runningpower = runningpower - 1;
     }
-    return (runningpower);
+    return (integer);
 }
