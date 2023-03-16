9b27755fcf5eef77bb14fe446d9d407c16f6104d
Christian <christian.goiak@outlook.com>
2023-03-16 11:18:48 +1030

    dfas

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int integer = 0;
-int bin_to_int(int binary_digits[], int number_of_digits)
+int bin_to_int(int binary_digits[5], int number_of_digits)
 {
     int runningpower = number_of_digits;
     for (int i = 0; i < number_of_digits; i++)
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <function-2-2.cpp>
+extern int bin_to_int(int binary_digits[5], int number_of_digits);
 int main()
 {
-    int binary_digits[5] = [10011];
-    int number_of_digits = sizeof(binary_digits);
-    int bin_to_int(int binary_digits[], int number_of_digits);
-
+    int binary_digits[5] = {1, 0, 0, 1, 1};
+    int number_of_digits = 5;
+    int number = bin_to_int(binary_digits, number_of_digits);
+    printf("%d", number);
     return 0;
 }
