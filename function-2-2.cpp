0ae95c2b50dc820812f2c844e091ffa8eef67c72
Christian <christian.goiak@outlook.com>
2023-03-16 11:33:42 +1030

    kzfhslkdhf

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <cmath>
 int integer = 0;
 int bin_to_int(int binary_digits[5], int number_of_digits)
 {
     int runningpower = number_of_digits - 1;
     for (int i = 0; i < number_of_digits; i++)
     {
-        integer = integer + (binary_digits[i] * (2 ^ runningpower));
+        integer = integer + (binary_digits[i] * (pow(2, runningpower)));
         runningpower = runningpower - 1;
     }
     return (integer);
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -2,7 +2,7 @@
 extern int bin_to_int(int binary_digits[5], int number_of_digits);
 int main()
 {
-    int binary_digits[5] = {1, 1, 1, 1, 1};
+    int binary_digits[5] = {1, 0, 0, 0, 1};
     int number_of_digits = 5;
     int number = bin_to_int(binary_digits, number_of_digits);
     printf("%d", number);
